438ab1dad09ec56dd01db6bac92113663ccbe24e
juliepearcy <juliepearcy@byui.edu>
2021-01-30 16:56:22 -0800

    validate

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,5 +1,23 @@
 
 
+//current year
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {const responsive = 'responsive';
+mainnav.classList.toggle(responsive)}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+<ul class="navigation">***/
+              
+
+
 let x = new Date();
 let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -8,7 +26,5 @@ document.getElementById("dateDisplay").innerHTML = day[x.getDay()] + ", " +
 x.getDate() + " " + month[x.getMonth()] + " " + x.getFullYear();
 
 
-function smallMenu() {
-    document.getElementById("navigation").classList.toggle("disappear");
-}
-       
+
+
