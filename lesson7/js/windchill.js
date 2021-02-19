e781e8cb3c5bb2e4f0e0a4b5a2a89ffe49889145
juliepearcy <juliepearcy@byui.edu>
2021-02-18 20:02:03 -0800

    resubmit

--- lesson7/js/windchill.js
+++ lesson7/js/windchill.js
@@ -6,3 +6,11 @@ let temp = document.getElementById('temp').innerHTML;
 let windchill = 35.74 + (0.6215*temp) - (35.75 * (Math.pow(windspeed,.16))) + (.4275*temp*(Math.pow(windspeed,.16)));
 windchill = parseInt(windchill);
 document.getElementById('windchill').innerHTML = windchill;
+
+if ((t > 50) || (s < 3)) {
+    document.getElementByID("chill").innerHTML = "NA";
+} else {
+    document.getElementById("chill").innerHTML = wc;
+}
+
+
