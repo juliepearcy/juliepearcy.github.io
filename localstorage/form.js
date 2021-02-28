3f8081b16deb916b24c4060b2ddbc425addb5aa4
juliepearcy <juliepearcy@byui.edu>
2021-02-27 16:05:26 -0800

    console practice

--- localstorage/form.js
+++ localstorage/form.js
@@ -1,33 +1,22 @@
+var nameField = document.getElementById("name");
+var emailField = document.getElementById("email");
+var feedbackField = document.getElementById("feedback");
 
-// form.js
+var saveButton = document.getElementById("saveButton");
+var retrieveButton = document.getElementById("retrieveButton");
 
-const formId = "save-later-form"; // ID of the form
-const url = location.href; //  href for the page
-const formIdentifier = `${url} ${formId}`; // Identifier used to identify the form
-const saveButton = document.querySelector("#save"); // select save button
-const alertBox = document.querySelector(".alert"); // select alert display div
-let form = document.querySelector(`#${formId}`); // select form
-let formElements = form.elements; // get the elements in the form
+function saveResponses() {
+	localStorage.setItem("name", nameField.value);
+	localStorage.setItem("email", emailField.value);
+   	localStorage.setItem("feedback", feedbackField.value);
+}
 
+saveButton.addEventListener("click", saveResponses);
 
-// form.js
+function retrieveResponses() {
+	nameField.value = localStorage.getItem("name");
+   	emailField.value = localStorage.getItem("email");
+    	feedbackField.value = localStorage.getItem("feedback");
+}
 
-...
-/**
- * This function populates the form
- * with data from localStorage
- *
- */
-const populateForm = () => {
-  if (localStorage.key(formIdentifier)) {
-    const savedData = JSON.parse(localStorage.getItem(formIdentifier)); // get and parse the saved data from localStorage
-    for (const element of formElements) {
-      if (element.name in savedData) {
-        element.value = savedData[element.name];
-      }
-    }
-    const message = "Form has been refilled with saved data!";
-    displayAlert(message);
-  }
-};
-document.onload = populateForm(); // populate the form when the document is loaded
+retrieveButton.addEventListener("click", function(e) { e.preventDefault(); retrieveResponses(); });
